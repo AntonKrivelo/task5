19c3aeca992e8f1cad8dbb4782055de06900b477
Anton Krivelo <krivelo2017@mail.ru>
2025-10-02 20:39:54 +0300

    asdasd

--- src/components/Verify/Verify.jsx
+++ src/components/Verify/Verify.jsx
@@ -10,7 +10,7 @@ export default function VerifyPage() {
       setMsg('Нет токена');
       return;
     }
-    fetch(`${process.env.REACT_APP_API_URL}/api/verify?token=${token}`)
+    fetch(`/api/verify?token=${token}`)
       .then((res) => res.text())
       .then(setMsg)
       .catch(() => setMsg('Ошибка верификации'));
